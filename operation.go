30c08116c97bf1500329f024d7167da95a439145
Matt Gleich <git@mattglei.ch>
2026-03-06 09:55:08 -0500

    feat: change Inherit to Extend

--- operation.go
+++ operation.go
@@ -2,6 +2,6 @@ package oplog
 
 type Operation string
 
-func (o Operation) Inherit(opt string) Operation {
+func (o Operation) Extend(opt string) Operation {
 	return Operation(string(o) + "," + opt)
 }
